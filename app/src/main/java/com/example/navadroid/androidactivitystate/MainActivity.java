aaffaf54dbdc45ed7e5c7d00c6466048cd7f6f67
navadon <navadon@gmail.com>
2017-09-26 21:32:42 +0700

    Fix overridden method

--- app/src/main/java/com/example/navadroid/androidactivitystate/MainActivity.java
+++ app/src/main/java/com/example/navadroid/androidactivitystate/MainActivity.java
@@ -1,49 +1,138 @@
 package com.example.navadroid.androidactivitystate;
 
+import android.content.res.Configuration;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-// Source: https://developer.android.com/guide/components/activities/activity-lifecycle.html
-// Source: https://stackoverflow.com/questions/151777/saving-android-activity-state-using-save-instance-state
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "Android Activity State";
+
+    // view
+    private EditText etString;
+    private TextView tvOutput;
+
+    private String myString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // Always call the superclass first
         setContentView(R.layout.activity_main);
 
+        etString = (EditText) findViewById(R.id.et_string);
+        tvOutput = (TextView) findViewById(R.id.tv_output);
+
         // Check whether we're recreating a previously destroyed instance
         if (savedInstanceState != null) {
             // Restore value of members from saved state
-        } else {
-            // Probably initialize members with default values for a new instance
+            myString = savedInstanceState.getString("mySavedString");
+            Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+            Log.d(TAG, "onCreate() called with: myString = [" + myString + "]");
         }
-
+       else {
+            // Probably initialize members with default values for a new instance
+            myString = null;
+            Log.d(TAG, "onCreate() called with: myString = [" + myString + "]");
+        }
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+    protected void onSaveInstanceState(Bundle outState) {
         // Always call the superclass so it can save the view hierarchy state
-        super.onSaveInstanceState(outState, outPersistentState);
+        super.onSaveInstanceState(outState);
 
         // Save UI state changes to the savedInstanceState.
         // This bundle will be passed to onCreate if the process is
         // killed and restarted.
 
         //outState.putVarType("VarName", "VarValue");
-
+        outState.putString("mySavedString", myString);
+        Log.d(TAG, "onSaveInstanceState() called with: outState = [" + outState + "]");
     }
 
     @Override
-    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         // Always call the superclass so it can restore the view hierarchy
-        super.onRestoreInstanceState(savedInstanceState, persistentState);
+        super.onRestoreInstanceState(savedInstanceState);
 
         // Restore UI state from the savedInstanceState.
         // This bundle has also been passed to onCreate.
 
-        //VarType varName = savedInstanceState.getVarType("VarName");
+        myString = savedInstanceState.getString("mySavedString"); //VarType varName = savedInstanceState.getVarType("VarName");
+        Log.d(TAG, "onRestoreInstanceState() called with: savedInstanceState = [" + savedInstanceState + "]");
+        Log.d(TAG, "onRestoreInstanceState() called with: myString = [" + myString + "]");
+    }
+
+    public void process(View view){
+        switch (view.getId()) {
+            case R.id.btn_save:
+                myString = etString.getText().toString();
+                etString.setText("");
+                etString.setHint("Type something here");
+                break;
+            case R.id.btn_show:
+                if (myString != null)
+                    tvOutput.setText(myString);
+                else
+                    tvOutput.setText("No saved string.");
+                break;
+            case R.id.btn_clear:
+                resetView();
+                break;
+        }
+    }
+
+    private void resetView(){
+        etString.setText("");
+        etString.setHint("Type something here");
+        tvOutput.setText("");
+    }
+
+    // called when the activity is about to become visible
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
+    }
+
+    // called when the activity has become visible
+    @Override
+    protected void onResume() {
+        super.onResume();
+        resetView();
+        Log.d(TAG, "onResume() called");
+    }
+
+    // called when another activity is taking focus
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    // called when the activity is no longer visible
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    // called just before the activity is destroyed
+    // Configuration changes during runtime (screen orientation, keyboard availability, and language) also call onDestroy
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
